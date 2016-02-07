c1a885b256460adaef57ebb876a87644319e9276
Alex Michael <hi@alexmic.net>
2016-02-07 17:16:18 +0000

    Ensure URL to crawl is absolute and contains a trailing slash

--- sitemapper.go
+++ sitemapper.go
@@ -4,6 +4,9 @@ import (
 	"github.com/alexmic/sitemapper/crawl"
 	"github.com/codegangsta/cli"
 	"os"
+    "log"
+    "strings"
+    "net/url"
 )
 
 func main() {
@@ -13,9 +16,25 @@ func main() {
 	app.Usage = "a crawler for building sitemaps"
 
 	app.Action = func(c *cli.Context) {
-		url := c.Args().Get(0)
+		rawurl := c.Args().Get(0)
+        if rawurl == "" {
+            log.Fatal("Please give a URL to crawl.")
+            os.Exit(1)
+        }
+        url, err := url.Parse(rawurl)
+        if err != nil {
+            log.Fatal(err)
+            os.Exit(1)
+        }
+        if url.Scheme == "" {
+            url.Scheme = "http"
+        }
+        absUrl := url.String()
+        if !strings.HasSuffix(absUrl, "/") {
+            absUrl += "/"
+        }
 		crawler := crawl.NewCrawler(2)
-		sitemap, _ := crawler.Crawl(url)
+		sitemap, _ := crawler.Crawl(absUrl)
 		sitemap.PrettyPrint()
 	}
 
